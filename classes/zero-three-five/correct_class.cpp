1636b88f2d4c225d410fc30823943f8c5c2cd895
James Collerton <jamescollerton@live.co.uk>
2024-10-21 18:43:19 +0100

    Moving to string initializer

--- classes/zero-three-five/correct_class.cpp
+++ classes/zero-three-five/correct_class.cpp
@@ -3,6 +3,7 @@
 
 CorrectClass::CorrectClass(std::string *string_pointer): _string_pointer(string_pointer) {
     std::cout << "Calling normal constructor" << std::endl;
+    _string_pointer = new std::string("A");
 }
 
 void CorrectClass::print() {
--- classes/zero-three-five/main.cpp
+++ classes/zero-three-five/main.cpp
@@ -4,7 +4,12 @@
 int main() {
 
     std::string cc_string = "Correct class";
-    CorrectClass correct_class(&cc_string);
+    std::string *cc_string_pointer = &cc_string;
+    CorrectClass correct_class(cc_string_pointer);
+    
+    std::string cc_string_2 = "Correct class 2";
+    cc_string_pointer = &cc_string_2;
+
     correct_class.print();
 
     return 0;
